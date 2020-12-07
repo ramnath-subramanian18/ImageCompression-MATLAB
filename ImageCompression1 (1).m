55ba5ccbf38d69ea7790e8c338f357859da76828
Ramnath <47588794+sramnath123@users.noreply.github.com>
2020-12-07 17:09:36 +0530

    Update ImageCompression1 (1).m

--- ImageCompression1 (1).m
+++ ImageCompression1 (1).m
@@ -1,30 +1,4 @@
 function varargout = ImageCompression1(varargin)
-% IMAGECOMPRESSION1 MATLAB code for ImageCompression1.fig
-%      IMAGECOMPRESSION1, by itself, creates a new IMAGECOMPRESSION1 or raises the existing
-%      singleton*.
-%
-%      H = IMAGECOMPRESSION1 returns the handle to a new IMAGECOMPRESSION1 or the handle to
-%      the existing singleton*.
-%
-%      IMAGECOMPRESSION1('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in IMAGECOMPRESSION1.M with the given input arguments.
-%
-%      IMAGECOMPRESSION1('Property','Value',...) creates a new IMAGECOMPRESSION1 or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before ImageCompression1_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to ImageCompression1_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help ImageCompression1
-
-% Last Modified by GUIDE v2.5 15-Oct-2014 22:20:56
-
-% Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
@@ -41,49 +15,28 @@ if nargout
 else
     gui_mainfcn(gui_State, varargin{:});
 end
-% End initialization code - DO NOT EDIT
 
-
-% --- Executes just before ImageCompression1 is made visible.
 function ImageCompression1_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to ImageCompression1 (see VARARGIN)
 
-% Choose default command line output for ImageCompression1
 handles.output = hObject;
 
-% Update handles structure
 guidata(hObject, handles);
 guidata(hObject, handles);
 set(handles.axes1,'visible','off')
 set(handles.axes2,'visible','off')
 axis off
 axis off
-% UIWAIT makes ImageCompression1 wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
 
 
-% --- Outputs from this function are returned to the command line.
 function varargout = ImageCompression1_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 
-% Get default command line output from handles structure
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in pushbutton1.
 function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+
 global file_name;
-%guidata(hObject,handles)
+
 file_name=uigetfile({'*.bmp;*.jpg;*.png;*.tiff;';'*.*'},'Select an Image File');
 fileinfo = dir(file_name);
 SIZE = fileinfo.bytes;
@@ -91,20 +44,14 @@ Size = SIZE/1024;
 set(handles.text7,'string',Size);
 imshow(file_name,'Parent', handles.axes1)
 
-% --- Executes on button press in pushbutton2.
+
 function pushbutton2_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% hObject    handle to pushbutton2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+
 global file_name;
 if(~ischar(file_name))
    errordlg('Please select Images first');
 else
-    I1 = imread(file_name);
-% I1 = imread('chicken.jpg');
+    I1 = imread('image.jpg');
 I = I1(:,:,1);
 I = im2double(I);
 T = dctmtx(8);
